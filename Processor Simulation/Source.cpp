7a76938c9a7b0d789c71515087007461a955e358
jdg313 <jgehr@umich.edu>
2024-04-06 15:09:14 -0400

    update 1 main processor simulation works, still needs to add priority and metrics

--- Processor Simulation/Source.cpp
+++ Processor Simulation/Source.cpp
@@ -18,72 +18,89 @@ struct jobEntry {
     char jobType;
     int arrivalTime;
     int processingTime;
-    jobEntry* next;
+    int jobNum;
     // default constructor
-    jobEntry() : jobType('\0'), arrivalTime(0), processingTime(0), next(nullptr) {}
+    jobEntry() : jobType('\0'), arrivalTime(0), processingTime(0), jobNum(-1){}
     // parameterized constructor
-    jobEntry(char jt, int at, int pt, jobEntry* ptr) : jobType(jt), arrivalTime(at),
-    processingTime(pt), next(ptr) {}
+    jobEntry(char jt, int at, int pt, int jn) : jobType(jt), arrivalTime(at),
+    processingTime(pt), jobNum(jn) {}
+};
+
+struct processor {
+    bool isBusy = false;
+    jobEntry currentJob;
+    int timeJobStarted = 0;
 };
 
 struct jobComparator {
     bool operator()(const jobEntry& a, const jobEntry& b) const {
-        // Priority logic: earlier arrivalTime or higher priority for same arrivalTime
-        if (a.arrivalTime == b.arrivalTime) return a.jobType < b.jobType; // Assuming 'D' > 'A', 'B', 'C'
+        if (a.arrivalTime == b.arrivalTime) return a.jobType > b.jobType; // 'D' has the highest priority on equal arrivalTime
         return a.arrivalTime > b.arrivalTime;
     }
 };
 
-int main() {
-    int clock = 0;
-    ifstream inputData("mergedData.txt");
+void simPriorityQueue(priority_queue<jobEntry, vector<jobEntry>, jobComparator>& queue, int numOfProcessors);
 
-    // Create a linked list of all jobs from input data
-    jobEntry* head = nullptr;
-    jobEntry* tail = nullptr;
-    jobEntry* temp = nullptr;
+int main() {
+    ifstream inputData("mergedData.txt");
+    priority_queue<jobEntry, vector<jobEntry>, jobComparator> queue;
 
     char jobType;
-    int arrivalTime, processingTime;
+    int arrivalTime, processingTime, numOfProcessors;
+    int jobNum = 1;
 
     while (inputData >> jobType >> arrivalTime >> processingTime) {
-        jobEntry* newEntry = new jobEntry(jobType, arrivalTime, processingTime, nullptr);
-
-        if (head == nullptr) {
-            head = newEntry;
-            temp = newEntry;
-        }
-        else {
-            temp->next = newEntry;
-            temp = newEntry;
-        }
+        jobEntry newEntry(jobType, arrivalTime, processingTime, jobNum);
+        queue.push(newEntry);    
+        jobNum++;
     }
-    tail = temp;
-    temp = nullptr;
-    delete temp;
 
-    jobEntry* current = head;
-    while (current != nullptr) {
-        cout << "Type: " << current->jobType << endl;
-        cout << "Arrival Time: " << current->arrivalTime << endl;
-        cout << "Processing Time: " << current->processingTime << endl;
+    numOfProcessors = 4;
 
-        current = current->next;
-    }    
+    simPriorityQueue(queue, numOfProcessors);
+}
 
-    priority_queue<jobEntry*, vector<jobEntry*>, jobComparator> queue;
+void simPriorityQueue(priority_queue<jobEntry, vector<jobEntry>, jobComparator>& queue, int numOfProcessors) {
+    int clock = 0;
+    vector<processor> processors(numOfProcessors);
 
-    while (clock < 100) {
-        if (queue.empty()) {
-            
+    while (clock < 550) {
+        cout << "Time " << clock << ":\n";
+
+        for (int i = 0; i < processors.size(); ++i) {
+            auto& processor = processors[i];
+            // Check if the processor is busy and if the current job has finished
+            if (processor.isBusy && (clock - processor.timeJobStarted >= processor.currentJob.processingTime)) {
+                cout << "Processor #" << (i + 1) << " finished task " << processor.currentJob.jobNum <<
+                "(" << processor.currentJob.jobType << ")\n";
+                processor.isBusy = false; // Mark the processor as idle after finishing the job
+            }
+            else if (processor.isBusy) {
+                // If the processor is busy but the job hasn't finished yet, print the remaining time
+                cout << "Processor #" << (i + 1) << " is busy with task " << processor.currentJob.jobNum <<
+                "(" << processor.currentJob.jobType << "), " 
+                << processor.currentJob.processingTime - (clock - processor.timeJobStarted) << " minutes remaining\n"; 
+            }
+
+            // Assign new jobs to idle processors, considering their arrival times
+            if (!processor.isBusy && !queue.empty()) {
+                auto nextJob = queue.top(); // Look at the next job without removing it
+                if (nextJob.arrivalTime <= clock) { // Check if the job has arrived
+                    queue.pop(); // Remove the job from the queue since it's starting
+                    processor.currentJob = nextJob; // Assign the job to the processor
+                    processor.isBusy = true;
+                    processor.timeJobStarted = clock; // Record the start time of the job
+                    cout << "Processor #" << (i + 1) << " started task " << 
+                    nextJob.jobNum << "(" << nextJob.jobType << "),"
+                    << " with " << nextJob.processingTime << " minutes of processing time\n";
+                }
+            }
+
+            // Note: The condition for processor idle status is removed because it's implicitly covered
+            // by the conditions above. If a processor is idle and there are no jobs in the queue,
+            // there's no need for an additional message every clock tick.
         }
-    }
-    
-    // Delete and free memory once done
-    current = head;
-    while (current != nullptr) {
-        jobEntry* toDelete = current;
-        current = current->next;
-        delete toDelete;
+
+        clock++; // Advance the simulation time
     }
 }
